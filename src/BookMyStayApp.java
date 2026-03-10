37c9c9d729e5809d6235622fb22a05c723de489a
Anshsingh-007 <as9074@srmist.edu.in>
2026-03-10 09:55:12 +0530

    UC1: Add welcome message

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -1,2 +1,8 @@
 public class BookMyStayApp {
+
+    public static void main(String[] args) {
+        // UC1: Welcome message
+        System.out.println("Welcome to BookMyStay App v1.0");
+        System.out.println("Enjoy your stay booking experience!\n");
+    }
 }
