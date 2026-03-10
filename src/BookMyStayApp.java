6c6caddf918a12a9f0ca57a5743866fe4b97c44c
Anshsingh-007 <as9074@srmist.edu.in>
2026-03-10 09:56:14 +0530

    UC2: Add hardcoded room types

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -1,8 +1,34 @@
+// UC2: Hardcoded room types
+class Room {
+    String type;
+    int beds;
+    int price;
+
+    Room(String type, int beds, int price) {
+        this.type = type;
+        this.beds = beds;
+        this.price = price;
+    }
+
+    void displayRoom() {
+        System.out.println(type + " Room | Beds: " + beds + " | Price: $" + price);
+    }
+}
+
 public class BookMyStayApp {
 
     public static void main(String[] args) {
-        // UC1: Welcome message
-        System.out.println("Welcome to BookMyStay App v1.0");
-        System.out.println("Enjoy your stay booking experience!\n");
+        // UC1
+        System.out.println("Welcome to BookMyStay App v1.0\n");
+
+        // UC2
+        Room single = new Room("Single", 1, 50);
+        Room doubleRoom = new Room("Double", 2, 90);
+        Room suite = new Room("Suite", 3, 150);
+
+        single.displayRoom();
+        doubleRoom.displayRoom();
+        suite.displayRoom();
+        System.out.println();
     }
 }
