3282375a66a4e713d59a084ae55a8612128195f9
Anshsingh-007 <as9074@srmist.edu.in>
2026-03-10 10:02:48 +0530

    UC5: Add booking request queue

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -1,4 +1,14 @@
-import java.util.HashMap;
+import java.util.*;
+
+class Reservation {
+    String guestName;
+    String roomType;
+
+    Reservation(String guestName, String roomType) {
+        this.guestName = guestName;
+        this.roomType = roomType;
+    }
+}
 
 public class BookMyStayApp {
 
@@ -21,14 +31,26 @@ public class BookMyStayApp {
         inventory.put("Double", 5);
         inventory.put("Suite", 2);
 
-        // UC4: Room Search
-        System.out.println("Available Rooms for Booking:");
+        // UC4
         Room[] rooms = {single, doubleRoom, suite};
+        System.out.println("Available Rooms:");
         for (Room room : rooms) {
             if (inventory.get(room.type) > 0) {
                 room.displayRoom();
                 System.out.println("Available: " + inventory.get(room.type));
             }
+        }
+        System.out.println();
+
+        // UC5: Booking Request Queue
+        Queue<Reservation> bookingQueue = new LinkedList<>();
+        bookingQueue.offer(new Reservation("Alice", "Single"));
+        bookingQueue.offer(new Reservation("Bob", "Double"));
+        bookingQueue.offer(new Reservation("Charlie", "Suite"));
+
+        System.out.println("Booking requests queued (FIFO order):");
+        for (Reservation res : bookingQueue) {
+            System.out.println(res.guestName + " requested " + res.roomType);
         }
         System.out.println();
     }
