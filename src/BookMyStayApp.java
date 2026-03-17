3553a0a19341c7fbe5a894ea55037dc2202ccb67
Anshsingh-007 <as9074@srmist.edu.in>
2026-03-17 10:39:05 +0530

    UC12

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -1,2 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+class Reservation implements Serializable {
+    String id;
+
+    Reservation(String id) {
+        this.id = id;
+    }
+}
+
 public class BookMyStayApp {
+    public static void main(String[] args) {
+
+        // SAVE
+        try {
+            ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("data.ser"));
+            oos.writeObject(new Reservation("R1"));
+            oos.close();
+        } catch (Exception e) {
+            System.out.println("Save error");
+        }
+
+        // LOAD
+        try {
+            ObjectInputStream ois = new ObjectInputStream(new FileInputStream("data.ser"));
+            Reservation r = (Reservation) ois.readObject();
+            System.out.println("Loaded: " + r.id);
+            ois.close();
+        } catch (Exception e) {
+            System.out.println("Load error");
+        }
+    }
 }
