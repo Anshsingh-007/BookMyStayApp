516c93387a15d9e2c7abcdb340dd1df85dcff9c8
Anshsingh-007 <as9074@srmist.edu.in>
2026-03-17 10:37:12 +0530

    UC11

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -1,2 +1,39 @@
+import java.util.*;
+
+class Inventory {
+    Map<String, Integer> rooms = new HashMap<>();
+
+    Inventory() {
+        rooms.put("Deluxe", 1);
+    }
+
+    synchronized void book(String type) {
+        if (rooms.get(type) > 0) {
+            System.out.println(Thread.currentThread().getName() + " booked");
+            rooms.put(type, rooms.get(type) - 1);
+        } else {
+            System.out.println(Thread.currentThread().getName() + " failed");
+        }
+    }
+}
+
+class Task extends Thread {
+    Inventory inv;
+
+    Task(Inventory inv) {
+        this.inv = inv;
+    }
+
+    public void run() {
+        inv.book("Deluxe");
+    }
+}
+
 public class BookMyStayApp {
+    public static void main(String[] args) {
+        Inventory inv = new Inventory();
+
+        new Task(inv).start();
+        new Task(inv).start();
+    }
 }
